f7194f260ed544fdf731903d75978ed8bf860eef
Daniel Lechner <daniellechner91@hotmail.com>
2024-04-17 19:10:15 +0200

    Read txt-file

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(application.assets.open("test.txt").bufferedReader().use { it.readText() })
                 }
             }
         }
